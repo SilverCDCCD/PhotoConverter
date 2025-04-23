7dbc36764e4fa22236049f1b35bd54cce712231b
SilverCDCCD <164558667+SilverCDCCD@users.noreply.github.com>
2025-04-23 02:24:12 -0400

    Files added
    Files added for initial commit.

---
+++ ImageConverter/main.py
@@ -0,0 +1,24 @@
+import os
+from PIL import Image
+
+
+def save_updated(img_name: str, image_format: str, rotate: float = 0, resize: tuple[int, int] = (0, 0)):
+	with Image.open(img_name) as image:
+		new_file_name = ""
+		try:
+			dot_index = img_name.rindex(".")
+			new_file_name = img_name[len(old_directory)+1:dot_index]
+		except ValueError:
+			new_file_name = img_name[len(old_directory)+1:]
+		finally:
+			width = resize[0] if resize[0] > 0 else image.size[0]
+			height = resize[1] if resize[1] > 0 else image.size[1]
+			new_file_name = f"{new_file_name}_rot{rotate:03}_{width:04}x{height:04}"
+			image.rotate(rotate).resize((width, height)).save(f"{new_directory}/{new_file_name}.{image_format}")
+
+
+old_directory = "./originals"
+new_directory = "./updated"
+files = [f for f in os.listdir(old_directory) if os.path.isfile(f"{old_directory}/{f}")]
+for f in files:
+	save_updated(f"{old_directory}/{f}", "jpeg", 90, (128, 128))
